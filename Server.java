c28a6621680e585672fce092a0b460f5abb3fe09
Pathiraja-D <pathirajadnew@gmail.com>
2024-02-18 15:52:40 +0530

    correction on server

--- Server.java
+++ Server.java
@@ -46,26 +46,22 @@ public class Server{
 
             @Override
             public void mousePressed(MouseEvent e) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+                
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
+                
             }
 
             @Override
             public void mouseEntered(MouseEvent e) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
+                
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+                
             }
         
         };
